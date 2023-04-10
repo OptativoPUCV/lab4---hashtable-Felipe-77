9a678c73af876de5fa11d6f1b7f97d119c217e11
Your Name <you@example.com>
2023-04-10 13:46:19 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,22 +41,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
 
-    Pair * newPair = (Pair *) malloc(sizeof(Pair));
-    if (newPair == NULL) exit(EXIT_FAILURE);
-    newPair->key = key;
-    newPair->value = value;
-
-    long index = hash(key, map->capacity);
     
-
-    while (map->buckets[index] != NULL && map->buckets[index]->key != NULL){
-        
-        if (is_equal(key, map->buckets[index]->key) == 1) return;
-        index++;
-    }
-
-    map->buckets[index] = newPair;
-    map->size += 1;
 }
 
 void enlarge(HashMap * map) {
