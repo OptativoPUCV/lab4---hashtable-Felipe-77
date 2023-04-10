f8448ffc307d802392befa0a005eed2d68d37769
Your Name <you@example.com>
2023-04-10 17:32:52 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -63,13 +63,13 @@ void insertMap(HashMap * map, char * key, void * value) {
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
-    Pair ** aux = (Pair **) malloc(sizeof(Pair *) * map->capacity);
+    Pair ** aux = (Pair **) malloc(sizeof(Pair *));
     if (aux == NULL) exit(EXIT_FAILURE);
     aux = map->buckets;
 
     map->capacity *= 2;
     //map->buckets = (Pair **) calloc(map->capacity, sizeof(Pair *));
-    map->size = 0;
+    //map->size = 0;
 
 }
 
