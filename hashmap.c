9593cb95a103f4559f4a9194105d462b31c2a726
Your Name <you@example.com>
2023-04-10 16:41:14 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -85,7 +85,20 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
 
+    long index = hash(key, map->capacity);
+    while (map->buckets[index] != NULL && map->buckets[index]->key != NULL){
+        
+        if (is_equal(key, map->buckets[index]->key) == 1){
+            map->buckets[index]->key = NULL;
+            return;
+        }
+        
+        index++;
 
+        if (index >= map->size)
+            index %= map->capacity;
+    }
+    return ;
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
